ae3829751130bd5078cee4efa55e74df873e9b32
Greg <gfitzpatrick2@gmail.com>
2020-03-22 12:56:00 -0400

    remove the now ignored node_modules

--- src/app.js
+++ src/app.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const Product = require('./database/models/product');
 
-const app =express();
+const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    Product.find()
-        .then(products => {
-            res.status(200).send(products);
-        })
-        .catch(err => {
-            res.status(500).send(err);
-        })
-    
+  Product.find()
+    .then((products) => {
+      res.status(200).send(products);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 });
 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`server listening on ${PORT}`);
+  console.log(`server listening on ${PORT}`);
 });
 
 module.exports = app;
--- src/database/models/product.js
+++ src/database/models/product.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 require('../mongoose');
 
 const Product = mongoose.model('Product', {
-    name: {
-        type: String
-    }
+  name: {
+    type: String,
+  },
 });
 
 module.exports = Product;
