03c3ba438c6ec9620b9279516f9278e6e3a44117
Greg <gfitzpatrick2@gmail.com>
2020-05-24 10:48:49 -0400

    categories store actions added, getting categories in ProductCategories.vue

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const productRouter = require('./routes/products/controller');
 const userRouter = require('./routes/users/controller');
 const categoryRouter = require('./routes/categories/controller');
@@ -8,6 +9,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(express.json());
+app.use(cors());
 app.use('/products', productRouter);
 app.use('/users', userRouter);
 app.use('/categories', categoryRouter);
